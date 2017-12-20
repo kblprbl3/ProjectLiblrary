009c607589cead2bbc1370c90d80712023feb01e
kblprbl3 <kblprbl3@users.noreply.github.com>
2017-12-20 18:43:41 +0600

    ВОООТ)

---
+++ polls/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
+def index(request):
+     return render(request, 'polls/index.html',)
